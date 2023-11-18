6fbe87a82b4467f95076f592b99cc935253867b5
VvN123 <heero7_@hotmail.com>
2023-11-18 18:16:30 -0300

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'pagina2',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { QRCodeModule } from 'angularx-qrcode';
+
 
 
 @NgModule({
@@ -12,7 +14,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    QRCodeModule
   ],
   declarations: [HomePage]
 })
