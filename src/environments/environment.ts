f9f1d793b13e99d9fe8c5d330e0eb59fbac03196
VvN123 <heero7_@hotmail.com>
2023-11-18 17:30:27 -0300

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBAJUYLg_8H2k3hMeJE8bMPmOBLwQTdoBE",
+    authDomain: "ionicproyect-3fbbf.firebaseapp.com",
+    projectId: "ionicproyect-3fbbf",
+    storageBucket: "ionicproyect-3fbbf.appspot.com",
+    messagingSenderId: "520092126307",
+    appId: "1:520092126307:web:4ec9706c37e327be453c56"
+  }
 };
 
 /*
